392ed5b3891b71b0bf798557da6964477807f6fb
SuyogNe <suyogn78@gmail.com>
2025-02-19 21:41:23 +0545

    d6

---
+++ d6.py
@@ -0,0 +1,21 @@
+import turtle
+turtle.Screen().bgcolor("cyan")
+
+a=turtle.Screen()
+a.setup(500,500)
+
+turtle.title("Square")
+
+t=turtle.Turtle()
+t.color("red")
+t.fillcolor("yellow")
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+
+turtle.done()
